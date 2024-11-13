0aa8563e30a4c2420cc81fa7ebe5d2aaa0b262ba
Bikimaeh <98027908+ilnurkka@users.noreply.github.com>
2024-11-13 11:09:04 +0300

    доработка терминов и содержания

--- edit_audit.py
+++ edit_audit.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def edit_docx():
     file = request.files['file']
     file_content = file.read()
-    open('file_from_front.docx')
+    open('file_from_front.docx', mode='wb').write(file_content)
     document = Document(io.BytesIO(file_content))
 
     document_preparing(document)
